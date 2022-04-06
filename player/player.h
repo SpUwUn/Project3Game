bfe8e68d154745c76472e5cdc4fe863153a8c5d7
SpUwUn <59152080+SpUwUn@users.noreply.github.com>
2022-04-06 17:11:16 -0600

    Update player.h

--- player/player.h
+++ player/player.h
@@ -1,1 +1,16 @@
+#ifndef player_H
+#define player_H
 
+#include <iostream>
+using namespace std;
+
+class player{
+    private:
+        int pos[12][12];
+        float money;
+    public:
+        int move(char direction);
+        void enter();
+};
+
+#endif
