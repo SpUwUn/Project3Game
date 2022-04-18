fa093971f15f4c4f5caee28446bdb6706c20cf64
SpUwUn <59152080+SpUwUn@users.noreply.github.com>
2022-04-18 16:35:14 -0600

    slight comment edit

--- classes/driver.cpp
+++ classes/driver.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main(){
     Map pearlStreet;
-    location locations[5]; //array of locations where the first dimension is the number of the locatiom amd the second is its location
+    location locations[5]; //array of locations where the first dimension is the index of the locatiom amd the second is its coordinates
     char locationsData[5][3] //1st dimensions is index, 2nd is position and map char
     npc npcs[5]; //array of npcs in which one will spawn at random each game
 }
