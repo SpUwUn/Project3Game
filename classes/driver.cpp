2532b54fa786ab32f6d0c72f1faeb853573d9a79
Jonathan Bayley <jonathanbayley239@gmail.com>
2022-04-18 17:26:12 -0600

    debugging

--- classes/driver.cpp
+++ classes/driver.cpp
@@ -2,7 +2,7 @@
 #include "npc.h"
 #include "location.h"
 #include "sigOther.h"
-//#include "map.h"
+#include "map.h"
 #include "player.h"
 #include <cassert>
 using namespace std;
@@ -10,7 +10,7 @@ using namespace std;
 int main(){
     Map pearlStreet;
     pearlStreet.displayMap();
-    location locations[5]; //array of locations where the first dimension is the index of the locatiom amd the second is its coordinates
-    char locationsData[5][3] //1st dimensions is index, 2nd is position and map char
-    npc npcs[5]; //array of npcs in which one will spawn at random each game
+    /*location locations[5]; //array of locations where the first dimension is the index of the locatiom amd the second is its coordinates
+    char locationsData[5][3]; //1st dimensions is index, 2nd is position and map char
+    npc npcs[5]; //array of npcs in which one will spawn at random each game*/
 }
