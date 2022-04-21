1e18505bcae3f251a8007f8f82d1c376a6e5b07f
Jonathan Bayley <jonathanbayley239@gmail.com>
2022-04-20 19:15:36 -0600

    map.h changes

--- classes/map.h
+++ classes/map.h
@@ -12,22 +12,14 @@ class Map
           static const int num_rows = 12;
           static const int num_cols = 12;
 
-
-          int playerPosition[2];
-          int npcPosition[2];
-          int numLocations;
-          char mapData[num_rows][num_cols];
-          char locationsData[5][3]; //1st dimensions is index, 2nd is position and map char
+          char locationsData[12][12]; //1st dimensions is index, 2nd is position and map char
           
      public :
           Map();
 
           void resetMap();
           void displayMap();
-
-          // setters
           void setMapData(int, int, char);
-       
 };
  
 #endif
